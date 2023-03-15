c63c99b668c4811664640260984ba992d285f6f8
NotoriuxX <manuelmeryz@gmail.com>
2023-03-15 11:37:23 -0300

    Initial commit

---
+++ Verificacion/login.php
@@ -0,0 +1,53 @@
+<?php
+
+session_start();
+
+include 'conexion.php';
+
+$correo = $_POST['correo'];
+$password = $_POST['contraseña'];
+
+$query = "SELECT * FROM usuarios WHERE correo_electronico = ?";
+$stmt = mysqli_prepare($conn, $query);
+
+mysqli_stmt_bind_param($stmt, 's', $correo);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $stored_password = $row['pass_usu'];
+    
+    if (password_verify($password, $stored_password)) {
+        $_SESSION['usuario'] = $correo;
+        switch ($row['rol']) {
+            case 'usuario_normal':
+                header('location: ../inicio.php');
+                break;
+
+            case 'administrador':
+                header('location: ../inicio_admin.php');
+                break;
+
+            default:
+                exit;
+        }
+    } else {
+        echo '
+        <script>
+            alert("Correo electrónico o contraseña incorrectos. Por favor, verifique los datos introducidos.");
+            window.location = "../login.php";
+        </script>
+        ';
+        exit();
+    }
+} else {
+    echo '
+        <script>
+            alert("Correo electrónico o contraseña incorrectos. Por favor, verifique los datos introducidos.");
+            window.location = "../login.php";
+        </script>
+        ';
+        exit();
+}
+?>
